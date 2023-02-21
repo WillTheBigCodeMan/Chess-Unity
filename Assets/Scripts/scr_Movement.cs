03f00e24b38b929081c47adcb9691bc6f179d29b
WillTheBigCodeMan <121814654+WillTheBigCodeMan@users.noreply.github.com>
2023-02-21 09:44:37 +0000

    Update scr_Movement.cs

--- Assets/Scripts/scr_Movement.cs
+++ Assets/Scripts/scr_Movement.cs
@@ -8,4 +8,17 @@ public class scr_Movement : MonoBehaviour
     private void Start() {
         createBoard = gameObject.GetComponent<scr_CreateBoard>();
     }
+    
+    public int[][] GetMoves(int[] board, bool isWhiteMove){
+        int[][] moves = new int[0][2];
+        for(int i = 0; i < board.length; i++){
+            if(board[i] != 0&&(isWhiteMove&&board[i]>=8)||(!isWhiteMove&&board[i]<8)){
+                moves.Concat(GetPieceMoves(board, i)).ToArray();
+            }
+        }
+    }
+    
+    public int[][] GetPieceMoves(int[] board, int pieceIndex){
+    
+    }
 }
